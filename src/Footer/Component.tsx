d96cdbc3efe42dd74d82a5f0f60bb5eaa69ce3db
khosbilegt <archerdoc13@gmail.com>
2025-07-18 14:08:40 +1000

    added footer styles

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,12 +1,10 @@
 import { getCachedGlobal } from '@/utilities/getGlobals'
-import Link from 'next/link'
-import React from 'react'
 
 import type { Footer } from '@/payload-types'
 
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 import { CMSLink } from '@/components/Link'
 import { Logo } from '@/components/Logo/Logo'
+import { Fragment } from 'react'
 
 export async function Footer() {
   const footerData: Footer = await getCachedGlobal('footer', 1)()
@@ -15,18 +13,25 @@ export async function Footer() {
 
   return (
     <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
-          <Logo />
-        </Link>
-
+      <div className="w-full px-4 py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
-          <nav className="flex flex-col md:flex-row gap-4">
+          <nav className="flex flex-col md:flex-row gap-4 md:items-center">
             {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
+              return (
+                <Fragment key={i}>
+                  <CMSLink className="text-white" {...link} />
+                  {i < navItems.length - 1 && (
+                    <span className="text-white hidden md:inline">|</span>
+                  )}
+                </Fragment>
+              )
             })}
           </nav>
+        </div>
+
+        <div className="flex gap-4">
+          <p>© PRiMAX Pty Ltd, 2025. All Rights Reserved.</p>
+          <p>Designed by Extrablack</p>
         </div>
       </div>
     </footer>
